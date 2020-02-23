86ea273920db3d68c1fb82227aec602f0b14174d
axelJacquet <axel.jacquet@hotmail.com>
2020-02-23 17:30:59 +0100

    test again

---
+++ credentials.js
@@ -0,0 +1,49 @@
+import com.hivext.api.core.utils.Transport;
+
+var name = "SBI-AJ891787";
+var password = "kXzZdjJ5URbr";
+
+var data = toJSON({
+  "auth": {
+    "identity": {
+      "methods": [
+        "password"
+      ],
+      "password": {
+        "user": {
+          "domain": {
+            "id": "default"
+          },
+          "name": name,
+          "password": password
+        }
+      }
+    },
+    "scope": {
+      "project": {
+        "domain": {
+          "id": "default"
+        },
+        "name": "some-name"
+      }
+    }
+  }
+});
+
+try {
+    new Transport().post("https://swiss-backup.infomaniak.com/identity/v3/credentials", data, {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+    });
+
+    return {
+      type: "success",
+      result: 0
+     };
+} catch (e) {
+    return {
+        type: "error",
+        result: 401
+        message: "unknown error: " + e
+    };
+}
