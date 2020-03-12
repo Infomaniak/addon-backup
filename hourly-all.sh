ba735ee3db66a3792278c426b9d32bfa2f23fd8a
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-03-12 11:14:20 +0100

    Create hourly-all.sh

---
+++ hourly-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+set -e
+
+. /tmp/openrc.sh
+
+eval "/usr/bin/restic backup --tag filesystem --one-file-system /"
+
+
+sleep 5
+
+restic unlock
+
+eval "/usr/bin/restic forget --tag filesystem --keep-hourly 24 --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --keep-yearly 3 --prune"
+
+
+> /tmp/Backups_plan
+
+/usr/bin/restic snapshots --no-lock > /tmp/Backups_plan
