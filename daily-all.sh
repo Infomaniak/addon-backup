0881690625b6ef0794c69d58263295f89748b006
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-03-12 15:07:25 +0100

    Update daily-all.sh

--- daily-all.sh
+++ daily-all.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-. /tmp/openrc.sh
+. /root/.config/swissbackup/openrc.sh
 
 eval "/usr/bin/restic backup --tag filesystem --one-file-system /"
 
@@ -15,6 +15,6 @@ restic unlock
 eval "/usr/bin/restic forget --tag filesystem --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --keep-yearly 3 --prune"
 
 
-> /tmp/Backups_plan
+> /root/.config/swissbackup/Backups_plan
 
-/usr/bin/restic snapshots --no-lock > /tmp/Backups_plan
+/usr/bin/restic snapshots --no-lock > /root/.config/swissbackup/Backups_plan
