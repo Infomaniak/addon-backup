3bdd523361803e5fdeddffbd95bd2887a1a314d1
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-04-12 10:17:06 -0700

    Update daily-all.sh

--- scripts/daily-all.sh
+++ scripts/daily-all.sh
@@ -12,15 +12,18 @@ touch /home/plan.json
 
 . /home/.config/swissbackup/openrc.sh
 
-eval "/usr/bin/restic backup --hostname $host --tag filesystem --one-file-system /"
+if ! eval "/usr/bin/restic backup --hostname $host --tag filesystem --one-file-system /"; then
 
+         restic unlock
+fi
 
 sleep 5
 
-restic unlock
-
-eval "/usr/bin/restic forget --tag filesystem --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --keep-yearly 3 --prune"
-
+if ! eval "/usr/bin/restic forget --tag filesystem --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --keep-yearly 3 --prune"; then
+         restic unlock
+         
+         restic rebuild-index
+fi
 
 > /home/plan.json
 
