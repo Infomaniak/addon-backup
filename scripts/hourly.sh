01a32a7966f74d4306c903bfa36bd0d6ca43df91
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-04-12 07:18:26 -0700

    Update hourly.sh

--- scripts/hourly.sh
+++ scripts/hourly.sh
@@ -33,37 +33,6 @@ for i in $@ ; do
         esac
 done
 
-usage2() {  1>&2; exit 1; }
-
-while getopts "s:h:d:w:m:y:" o; do
-    case "${o}" in
-
-        s)
-            last_snapshot=${OPTARG}
-            ;;
-
-
-        h)
-            hour=${OPTARG}
-            ;;
-        d)
-            day=${OPTARG}
-            ;;
-        w)
-            week=${OPTARG}
-            ;;
-
-        m)
-            month=${OPTARG}
-            ;;
-
-        y)
-            year=${OPTARG}
-            ;;
-    esac
-done
-shift $((OPTIND-1))
-
 FOLDERS_TO_BACKUP=$(echo ${FOLDER_TO_BACKUP} | tr -d  ' ' | tr  ',' ' ' )
 
 for i in ${FOLDERS_TO_BACKUP}"" ; do
@@ -74,28 +43,26 @@ done
 
 for p in ${FOLDERS_TO_BACKUP}"" ; do
 
-sleep 15
-restic unlock
+        sleep 5
 
-eval "/usr/bin/restic forget --tag $p --keep-hourly 24 --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --keep-yearly 3 --prune"
+        restic unlock
+
+        eval "/usr/bin/restic forget --tag $p --keep-hourly 24 --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --keep-yearly 3 --prune"
+
 done
-
-echo " last = ${last_snapshot}"
-echo " year = ${year}"
-
 
 > /home/plan.json
 
 function loopOverArray(){
 
-   restic snapshots --json | jq -r '.?' | jq -c '.[]'| while read i; do
+         restic snapshots --json | jq -r '.?' | jq -c '.[]'| while read i; do
          id=$(echo "$i" | jq -r '.| .short_id')
          ctime=$(echo "$i" | jq -r '.| .time' | cut -f1 -d".")
-         hostname=$(echo "$i" | jq -r '.| .hostname')
-        paths=$(echo "$i" | jq -r '. | .paths | join(",")')
+         paths=$(echo "$i" | jq -r '. | .paths | join(",")')
 
-        printf "id: %-25s - %-35s - %-25s paths: %-10s \n" $id $ctime $hostname $paths >> /home/plan.json
-         done
+        printf "id: %-15s - %-25s - paths: %-10s \n" $id $ctime $paths >> /home/plan.json
+
+done
   }
 
    loopOverArray
