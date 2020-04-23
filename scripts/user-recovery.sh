4e893c3aa3f60859475b166f215c6d39dbe6931f
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-04-23 02:59:52 -0700

    Update user-recovery.sh

--- scripts/user-recovery.sh
+++ scripts/user-recovery.sh
@@ -96,7 +96,7 @@ for i in ${TARGET_IDS}"" ; do
         echo " cet id est bon "
         eval "restic restore $i --target $RESTOREDIR"
         chown -R $user:$user $RESTOREDIR
-        cp -pR $RESTOREDIR/* $destination
+        cp -p $RESTOREDIR/* $destination
         rm -rf $RESTOREDIR
 
   fi
