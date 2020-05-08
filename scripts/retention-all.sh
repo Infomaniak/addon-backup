62745092d7f22ba220a9bf6869679402401f3ef8
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-05-08 05:13:56 -0700

    Update retention-all.sh

--- scripts/retention-all.sh
+++ scripts/retention-all.sh
@@ -2,6 +2,7 @@
 
 set -x
 set -e
+
 host=$(hostname -a)
 
 
@@ -11,46 +12,7 @@ touch /home/plan.json
 
 . /home/.config/swissbackup/openrc.sh
 
-usage2() {  1>&2; exit 1; }
-
-while getopts "h:d:m:y:" o; do
-    case "${o}" in
-
-        h)
-            hour=${OPTARG}
-            ;;
-        d)
-            day=${OPTARG}
-            ;;
-
-        m)
-            month=${OPTARG}
-            ;;
-
-        y)
-            year=${OPTARG}
-            ;;
-    esac
-done
-shift $((OPTIND-1))
-
-
-restic unlock
-
-if ! eval "/usr/bin/restic backup --hostname $host --tag filesystem --one-file-system /"; then
-
-         restic unlock
-fi
-
-sleep 5
-
-if ! eval "/usr/bin/restic forget --tag filesystem --keep-within "$year"y"$month"m"$day"d"$hour"h --prune"; then
-
-         restic unlock
-
-         restic rebuild-index
-
-fi
+eval "/usr/bin/restic backup --hostname $host --tag filesystem --one-file-system /"
 
 > /home/plan.json
 
@@ -66,7 +28,10 @@ function loopOverArray(){
                done
               }
              function parse(){
-              local res=$(loopOverArray)
-       echo "[$res]" | sed 's/\(.*\),/\1 /' >> /home/plan.json
-          }
+                  local res=$(loopOverArray)
+                  res_clean=$(echo "[$res]" | sed 's/\(.*\),/\1 /')
+                  now=`date +%s`
+                  myplan="{\"last_update\": \"$now\", \"backup_plan\":$res_clean}"
+                  echo $myplan >> /home/plan.json
+                  }
          parse
