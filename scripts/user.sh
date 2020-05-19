43ebdb65cf050c2a5cc26f6916ab00a5e710cdeb
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-05-19 05:33:11 -0700

    Update user.sh

--- scripts/user.sh
+++ scripts/user.sh
@@ -4,18 +4,18 @@ host=$(hostname -s)
 
 if type apt 2>/dev/null ; then
 	apt-get install -y curl
-  curl -d "email=$1&container=$host" -X POST http://10.4.38.87:8080/user
+  curl -d "email=$1&container=$host" -X POST http://jelastic-addon-swissbackup.infomaniak.ch:8080/user
 	echo "curl installed on debian"
 else
 	detect=$(cat /etc/*-release | grep 'ID=')
 	if [[ $detect == *"centos"* ]]; then
 		yum install curl
-    curl -d "email=$1&container=$host" -X POST http://10.4.38.87:8080/user
+    curl -d "email=$1&container=$host" -X POST http://jelastic-addon-swissbackup.infomaniak.ch:8080/user
 		echo "curl installed on centos"
 	elif [[ $detect == *"alpine"* ]]; then
 		apk add curl
 		echo "curl installed on alpine"
-    curl -d "email=$1&container=$host" -X POST http://10.4.38.87:8080/user
+    curl -d "email=$1&container=$host" -X POST http://jelastic-addon-swissbackup.infomaniak.ch:8080/user
 
 	fi
 fi
