8f993b6b4d7b3487b509030409403bd4f43c740a
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-07-20 12:18:44 +0200

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -46,6 +46,7 @@ ids.forEach(function(element) {
     } else {
         file = FileReadResponse.body;
         var plan = toNative(new Yaml().load(file));
+        jelastic.marketplace.console.WriteLog("ta mere" + typeof plan);
         if (!("last_update" in plan)){
             
             
