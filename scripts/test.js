07663cfd7bed29e7f1d1145de32241ec1527239b
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-07-20 10:05:06 +0200

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -47,10 +47,10 @@ ids.forEach(function(element) {
         file = FileReadResponse.body;
         var plan = toNative(new Yaml().load(file));
        try { 
-           if( plan ) {
+           if( plan ){
             }           
        }
-       catch () {
+       catch (err){
             return { type: "error", message: "Problem to load your Backup plan, contact Infomaniak support to solve issue" };
  
        }
