fa795cb7260146376c6a9beab44457381da3c5b0
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-12-13 16:09:51 +0100

    Update check.cs
    add new endpoint

--- scripts/check.cs
+++ scripts/check.cs
@@ -65,18 +65,25 @@ import org.apache.commons.httpclient.methods.StringRequestEntity;
           }), "application/json", "UTF-8");
 
           var post = new PostMethod("https://swiss-backup.infomaniak.com/identity/v3/auth/tokens");
+          var post2 = new PostMethod("https://swift02-api.cloud.infomaniak.ch/identity/v3/auth/tokens");
+
 
           post.setRequestEntity(requestEntity);
+          post2.setRequestEntity(requestEntity);
+
 
           var status = client.executeMethod(post);
+          var status2 = client.executeMethod(post2);
 
           post.releaseConnection();
+          post2.releaseConnection();
 
-          if (status == HttpStatus.SC_CREATED) { // 201
+
+          if (status == HttpStatus.SC_CREATED || status2 == HttpStatus.SC_CREATED ) { // 201
 
             return { result : 0 };
 
-          } else if (status == HttpStatus.SC_UNAUTHORIZED) { // 401
+          } else if (status == HttpStatus.SC_UNAUTHORIZED && status2 == HttpStatus.SC_UNAUTHORIZED ) { // 401
 
               return { type: "error", message: "The Swissbackup identifiers are not correct, please check the connection information in your emails" };
 
