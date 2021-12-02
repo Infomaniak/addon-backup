534da55110c7616957230ed8bb09ed67a7e0e30b
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2021-12-02 14:29:21 +0100

    Update check.cs
    add new swift cluster check in production

--- scripts/check.cs
+++ scripts/check.cs
@@ -66,33 +66,40 @@ import org.apache.commons.httpclient.methods.StringRequestEntity;
 
           var swift01 = "https://swiss-backup.infomaniak.com/identity/v3"
           var swift02 = "https://swift02-api.cloud.infomaniak.ch/identity/v3"
+          var swift03 = "https://swift03-api.cloud.infomaniak.ch/identity/v3"
+
           var post = new PostMethod("https://swiss-backup.infomaniak.com/identity/v3/auth/tokens");
           var post2 = new PostMethod("https://swift02-api.cloud.infomaniak.ch/identity/v3/auth/tokens");
-
+          var post3 = new PostMethod("https://swift03-api.cloud.infomaniak.ch/identity/v3/auth/tokens");
 
           post.setRequestEntity(requestEntity);
           post2.setRequestEntity(requestEntity);
+          post3.setRequestEntity(requestEntity);
 
 
           var status = client.executeMethod(post);
           var status2 = client.executeMethod(post2);
+          var status3 = client.executeMethod(post3);
 
           post.releaseConnection();
           post2.releaseConnection();
+          post3.releaseConnection();
 
 
-          if (status == HttpStatus.SC_CREATED || status2 == HttpStatus.SC_CREATED ) { // 201
+          if (status == HttpStatus.SC_CREATED || status2 == HttpStatus.SC_CREATED || status3 == HttpStatus.SC_CREATED ) { // 201
             if (status == HttpStatus.SC_CREATED) {
               return { result : 0, post: swift01}
             } else if (status2 == HttpStatus.SC_CREATED) {
               return { result : 0, post: swift02}
+            } else if (status3 == HttpStatus.SC_CREATED) {
+              return { result : 0, post: swift03}
             }
             return { result : 0 };
 
-          } else if (status == HttpStatus.SC_UNAUTHORIZED && status2 == HttpStatus.SC_UNAUTHORIZED ) { // 401
+          } else if (status == HttpStatus.SC_UNAUTHORIZED && status2 == HttpStatus.SC_UNAUTHORIZED && status3 == HttpStatus.SC_UNAUTHORIZED ) { // 401
 
               return { type: "error", message: "The Swissbackup identifiers are not correct, please check the connection information in your emails" };
 
           }
 
-          return { type: "error", message: "unknown error" };
+          return { type: "error", message: "unknown error", status: status3 };
