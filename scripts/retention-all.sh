0d7a0ed96ac06bfff7f105308c5e8de22b50466b
vdecarva <vlrdecarvalho@gmail.com>
2026-01-20 15:20:37 +0100

    Update retention-all.sh

--- scripts/retention-all.sh
+++ scripts/retention-all.sh
@@ -14,7 +14,7 @@ crontab -u root -l | grep -v '/var/log/first-backup.log'  | crontab -u root -
 eval "/usr/bin/restic backup --hostname $host --tag filesystem --one-file-system /"
 
 function loopOverArray(){
-         restic snapshots --hostname "$host" --json | jq -r '.?' | jq -c '.[]'| while read i; do
+         restic snapshots --hostname $host --json | jq -r '.?' | jq -c '.[]'| while read i; do
            id=$(echo "$i" | jq -r '.| .short_id')
            test=$(restic --no-lock stats $id | awk '{b=$3$4; print b}' |tail -1|sed 's/%$//g')
               size=$(echo $test)
