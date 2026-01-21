d291bfa4aa157e3694c7d56ea399ed3ca59dd5b0
vdecarva <vlrdecarvalho@gmail.com>
2026-01-21 16:06:19 +0100

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -23,11 +23,10 @@ for (var i = 0; envInfo = resp.infos[i]; i++) {
                         name: conteneur.substring(conteneur.indexOf('-') + 1, conteneur.length),
                         id: conteneur.substring(4, conteneur.indexOf('-'))
                     });
-                   
+                    // Virtuozzo versioning use a uniqueappid, which duplicate the number of backupTemplate matching in node object, hence being pushed to ids.
+                    // Also trying to stop once we reach a node with the addon, since plan file contains backup_plan for all node from an env.
+                    break;   
                 }
-            // Virtuozzo versioning use a uniqueappid, which duplicate the number of backupTemplate matching in node object, hence being pushed to ids.
-            // Also trying to stop once we reach a node with the addon, since plan file contains backup_plan for all node from an env.
-            break; 
             }
         }
     }
